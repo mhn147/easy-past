b73262b04f7a690ef4a515c425621664dc4822ca
mhn147 <muhammed.al.nasri@gmail.com>
2019-09-01 13:45:39 +0100

    add html skeleton

--- easy-past/src/app/app.component.ts
+++ easy-past/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import M from 'materialize-css';
 
 @Component({
-  selector: 'app-root',
+  selector: 'ep-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'easy-past';
+export class AppComponent implements OnInit {
+    title = 'easy-past';
+
+    ngOnInit(): void {
+        //initialization of the sidenav-trigger
+        document.addEventListener('DOMContentLoaded', function() {
+            var elems = document.querySelectorAll('.sidenav');
+            let options = {};
+
+            M.Sidenav.init(elems, options);
+          });
+    }
 }
